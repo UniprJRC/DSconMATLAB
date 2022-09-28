8670dfe02a08a20995147a3bc58d58dae3fc7d35
UniprJRC <FSDA@unipr.it>
2022-09-28 14:24:41 +0000

    🔄 Synced local 'matlabfiles/' with remote 'matlabMfiles/'

--- matlabfiles/capDistribuzioni/uniformeDisc.m
+++ matlabfiles/capDistribuzioni/uniformeDisc.m
@@ -21,3 +21,23 @@ title(['Grafico a barre di ' num2str(n) ' numeri generati ' ...
     'definita nell''insieme (' i1 ',' i2 ', ...,' in ')'])
 
 % print -depsc figs\uniformeDisc.eps;
+
+%% La parte di seguito non è nel testo
+% Il font del titolo è troppo piccolo. Era necessario suddividere il titolo
+% del grafico in titolo e sottotitolo come segue
+imin=11;
+imax=20;
+n=10000;
+x=randi([imin,imax],[n,1]);
+% la funzione tabulateFS di FSDA toolbox 
+% crea la distribuzione di frequenze delle modalità 
+% del vettore numerico x
+TB=tabulateFS(x);
+bar(TB(:,1),TB(:,2))
+i1=num2str(imin);
+i2=num2str(imin+1);
+in=num2str(imax);
+titolo=['Grafico a barre di ' num2str(n) ' numeri casuali '];
+sottotitolo=['generati da una distribuzione uniforme discreta definita nell''insieme (' i1 ',' i2 ', ...,' in ')'];
+title(titolo,sottotitolo)
+
