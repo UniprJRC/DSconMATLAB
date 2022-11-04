7cbb44842780d7e956d6cfc2f8cee40f5ad4bf55
UniprJRC <FSDA@unipr.it>
2022-11-04 16:43:37 +0000

    🔄 Synced local 'matlabfiles/' with remote 'matlabMfiles/'

--- matlabfiles/capAlgebra/proiezioneIntro.m
+++ matlabfiles/capAlgebra/proiezioneIntro.m
@@ -4,8 +4,13 @@ x=[1; 1; 0];
 v=[1; -1; 0];
 %t = x'*v/(v'v) 
 t =x'*v/(v'*v);
+% In questo esempio i vettori x e v sono ortogonali  (il loro prodotto
+% scalare è 0) di conseguenza t=0
 r_0=[0; 0; 0];
+% Il vettore hatx è semplicemente il punto di coordinate 0 0 0
 hatx= r_0+t*v;
 % dist= || x- hatx||
 dist=norm(x-hatx);
 disp(['La distanza è uguale a: ' num2str(dist)]) 
+% La distanza non è altro che la norma del vettore x (dato che hatx=0)
+% In altri termini, in questo esempio la distanza è la radice quadrata di 2
