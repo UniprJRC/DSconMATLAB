879483ecbef81f2a3d37412c6325de19340f290c
UniprJRC <FSDA@unipr.it>
2022-11-11 13:24:46 +0000

    🔄 Synced local 'matlabfiles/' with remote 'matlabMfiles/'

--- matlabfiles/MQF_II/F_DistNorm.m
+++ matlabfiles/MQF_II/F_DistNorm.m
@@ -12,32 +12,32 @@ function [x, medcamp, stdevcamp, stdevcamp_parz, skewncamp, skewncamp_parz, kurt
 %           sigma  := deviazione standard della distribuzione normale
 % OUTPUTS:  x      := vettore aleatoria simulato
 %           medcamp := media campionaria
-%           stdevcamp  := deviazione standard campionaria imparziale
-%           skewncamp  := l'indice di asimmetria campionaria imparziale
-%           kurtoscamp := l'indice di curtosi campionaria imparziale
-%           stdevcamp_parz  := deviazione standard campionaria parziale
-%           skewncamp_parz  := l'indice di asimmetria campionaria parziale
-%           kurtoscamp_parz := l'indice di curtosi campionaria parziale
+%           stdevcamp  := deviazione standard campionaria (imparziale)
+%           skewncamp  := l'indice di asimmetria campionaria (imparziale)
+%           kurtoscamp := l'indice di curtosi campionaria (imparziale)
+%           stdevcamp_parz  := deviazione standard campionaria (parziale)
+%           skewncamp_parz  := l'indice di asimmetria campionaria (parziale)
+%           kurtoscamp_parz := l'indice di curtosi campionaria (parziale)
 x = mu + sigma*randn(n,1);   % genera una variabile aleatoria normale
                              % da una normale standard con media 0 e
                              % deviazione standard 1
 medcamp = mean(x);       % media dei valori simulati x
 x0 = x-medcamp;          % gli scarti
-stdevcamp = sqrt(sum(x0.^2)/(n-1)); % deviazione standard imparziale
-stdevcamp_parz=sqrt((n-1)/n*sum(x0.^2)/(n-1));  % deviazione standard parziale
-skewncamp_parz = mean(x0.^3)/stdevcamp_parz^3; % l'indice di asimmetria parziale
-skewncamp=sqrt(n*(n-1))/(n-2)*skewncamp_parz; % l'indice di asimmetria imparziale
-kurtoscamp_parz = mean(x0.^4)/stdevcamp_parz^4;  % l'indice di curtosi imparziale
+stdevcamp = sqrt(sum(x0.^2)/(n-1)); % deviazione standard (imparziale)
+stdevcamp_parz=sqrt((n-1)/n*sum(x0.^2)/(n-1));  % deviazione standard (parziale)
+skewncamp_parz = mean(x0.^3)/stdevcamp_parz^3; % l'indice di asimmetria (parziale)
+skewncamp=sqrt(n*(n-1))/(n-2)*skewncamp_parz; % l'indice di asimmetria (imparziale)
+kurtoscamp_parz = mean(x0.^4)/stdevcamp_parz^4;  % l'indice di curtosi (imparziale)
 kurtoscamp = (n-1)/((n-2)*(n-3))*((n+1)*kurtoscamp_parz-3*(n-1))+3;  
-% l'indice di curtosi parziale
+% l'indice di curtosi (parziale)
 
 % Confronta le quantità calcolate precedentemente con le funzioni già
 % installate in Matlab.
 % Attenzione: Richiede l'installazione del toolbox "Statistics and Machine Learning"
-% dev = std(x, w); flag = 1 (default) == deviazione standard
-% parziale; flag = 0 == deviazione standard imparziale
+% dev = std(x, w); flag = 1 (default) == deviazione standard 
+% (imparziale); flag = 0 == deviazione standard (imparziale)
 % skewn = skewness(x, flag); % flag = 1 (default) == l'indice di asimmetria 
-% parziale; flag = 0 == l'indice di asimmetria imparziale     
+% (parziale); flag = 0 == l'indice di asimmetria (imparziale)     
 % kurtos = kurtosis(x, flag); % flag = 1 (default) == l'indice di curtosi 
-% parziale; flag = 0 == l'indice di curtosi imparziale        
+% (parziale); flag = 0 == l'indice di curtosi (imparziale)        
 end
