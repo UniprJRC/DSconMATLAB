0878b531dab98e906e67f563f27b83fceba2ab35
UniprJRC <FSDA@unipr.it>
2022-11-23 10:46:33 +0000

    🔄 Synced local 'matlabfiles/' with remote 'matlabMfiles/'

--- matlabfiles/capCorrispondenze/ScienzaTitStudio.m
+++ matlabfiles/capCorrispondenze/ScienzaTitStudio.m
@@ -14,40 +14,20 @@ out=CorAna(X(:,1:2),'datamatrix',true,'Lr',Lr,'Lc',Lc)
 
 % print -depsc scienzaedu.eps;
 
-%% 
-% Lr={'1= per niente favorevole' ...
-% '2= poco favorevole' ...
-% '3= indifferente' ...
-% '4= favorevole' ...
-% '5= molto favorevole'};
-% Lc={'1= licenza elementare' ...
-% '2= licenza media' ...
-% '3= diploma di scuola media superiore' ...
-% '4= laurea triennale' ...
-% '5= laurea specialistica' ...
-% '6= dottorato di ricerca'};
 
-
-%% In alternativa per legger i dati si poteva utilizzare
+%% In alternativa per leggere i dati si poteva utilizzare
 % Da notare che in questo caso Xchk un array e non una table 
-Xchk=xlsread('TURNO1soluzione.xlsx','dati','B2:E872');
-% Nelle release 2018 di MATLAB si possono utilizzare anche i simboli " "
-% invece dei simboli ' '
-% Xchk=xlsread("TURNO1soluzione.xlsx","dati","B2:E872");
+Xchk=xlsread('scienza.xlsx','dati','A2:B872','basic');
 
-% Dato che Xchk è au array 
-outchk=CorAna(Xchk(:,2:3),'datamatrix',true,'Lr',Lr,'Lc',Lc);
+% Dato che Xchk è un array 
+outchk=CorAna(Xchk,'datamatrix',true,'Lr',Lr,'Lc',Lc);
 
-% Per tracciare gli ellissi di confidenza
-
+%% Per tracciare gli ellissi di confidenza
+% Di seguito viene aggiunto l'ellisse di confidenza al punto riga "per
+% niente" ed al punto colonna 
 confellipse.selRows=1;
-confellipse.selCols=2;
+confellipse.selCols=1;
 confellipse.method={'multinomial'};
-CorAnaplot(outchk,'confellipse',confellipse)
+CorAnaplot(out,'confellipse',confellipse)
 
-%% Calcolo indice gamma (C-D)/(C+D)
-corrOrdinal(Xchk(:,2:3),'datamatrix',true)
 
-% Commento: concordanza di andamento tra "posizione verso la scienza" e
-% "titolo di studio".  Chi presenta un titolo di studio più alto tende ad
-% avere una posizione molto favorevole verso la scienza e viceversa
