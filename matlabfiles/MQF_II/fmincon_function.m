9eec4838303cbb959ec098dcb511afdc5a919c18
UniprJRC <FSDA@unipr.it>
2022-11-29 07:57:06 +0000

    🔄 Synced local 'matlabfiles/' with remote 'matlabMfiles/'

---
+++ matlabfiles/MQF_II/fmincon_function.m
@@ -0,0 +1,18 @@
+%% ESEMPIO DI PROBLEMA DI PROGRAMMAZIONE QUADRATICA CON fmincon
+Q = [ 1, -.5;
+    -.5,   1];      % parte quadratica della funzione obiettivo
+
+c = [-2,-6];        % parte lineare della funzione obiettivo
+
+fun = @(x) x'*Q*x+c*x; % funzione da minimizzare
+
+A = [ 1, 1;
+     -1, 2;
+      2, 1];
+b = [2; 2; 3];
+l_b = zeros(length(c),1);
+
+
+[x_asterisco, f_value] = fmincon(fun,[.5 .5]',A,b,[],[],l_b,[]); 
+f_x_asterisco=x_asterisco'*Q*x_asterisco+f*x_asterisco; % minimo della 
+%funzione obiettivo
