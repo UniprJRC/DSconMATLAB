a3ff46d765988403b8ff63e91c68db5fe08c78a5
UniprJRC <FSDA@unipr.it>
2023-07-06 15:13:41 +0000

    🔄 synced local 'matlabfiles/' with remote 'matlabMfiles/'

--- matlabfiles/capDistribuzioni/uniformeDisc.m
+++ matlabfiles/capDistribuzioni/uniformeDisc.m
@@ -3,28 +3,26 @@ rng(100)
 disp("Matrice 4x4 di numeri casuali interi dall'insieme (1, 2, ..., 8)")
 disp(randi(8,4))
 
-%% Numeri casuali uniformi dall'insieme 11, 12, ..., 20
-imin=11;
-imax=20;
-n=10000;
-x=randi([imin,imax],[n,1]);
-% la funzione tabulateFS di FSDA toolbox 
-% crea la distribuzione di frequenze delle modalità 
-% del vettore numerico x
-TB=tabulateFS(x);
-bar(TB(:,1),TB(:,2))
-i1=num2str(imin);
-i2=num2str(imin+1);
-in=num2str(imax);
-title(['Grafico a barre di ' num2str(n) ' numeri generati ' ...
-    'da una distribuzione uniforme discreta ' ...
-    'definita nell''insieme (' i1 ',' i2 ', ...,' in ')'])
+% %% Numeri casuali uniformi dall'insieme 11, 12, ..., 20
+% imin=11;
+% imax=20;
+% n=10000;
+% x=randi([imin,imax],[n,1]);
+% % la funzione tabulateFS di FSDA toolbox 
+% % crea la distribuzione di frequenze delle modalità 
+% % del vettore numerico x
+% TB=tabulateFS(x);
+% bar(TB(:,1),TB(:,2))
+% i1=num2str(imin);
+% i2=num2str(imin+1);
+% in=num2str(imax);
+% title(['Grafico a barre di ' num2str(n) ' numeri generati ' ...
+%     'da una distribuzione uniforme discreta ' ...
+%     'definita nell''insieme (' i1 ',' i2 ', ...,' in ')'])
 
 % print -depsc figs\uniformeDisc.eps;
 
-%% La parte di seguito non è nel testo
-% Il font del titolo è troppo piccolo. Era necessario suddividere il titolo
-% del grafico in titolo e sottotitolo come segue
+%% Numeri casuali uniformi dall'insieme 11, 12, ..., 20
 imin=11;
 imax=20;
 n=10000;
