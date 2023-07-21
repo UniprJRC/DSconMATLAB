668d1eb13bc054b5037a781c629244e3eca7c892
UniprJRC <FSDA@unipr.it>
2023-07-21 13:19:44 +0000

    🔄 synced local 'matlabfiles/' with remote 'matlabMfiles/'

--- matlabfiles/capCorrispondenze/ScienzaTitStudio.m
+++ matlabfiles/capCorrispondenze/ScienzaTitStudio.m
@@ -2,10 +2,16 @@
 X = readtable('scienza.xlsx','Sheet','dati');
 
 % Etichette delle righe e delle colonne della tabella di contingenza
-Lr={'1= per niente' '2= poco fav' '3= indiff' ...
-'4= fav' '5= molto fav'};
-Lc={'1= lic elem' '2= lic media' '3= diploma' ...
-'4= laurea tri' '5= laurea spe' '6= dottorat'};
+% Lr={'1= per niente' '2= poco fav' '3= indiff' ...
+% '4= fav' '5= molto fav'};
+% Lc={'1= lic elem' '2= lic media' '3= diploma' ...
+% '4= laurea tri' '5= laurea spe' '6= dottorat'};
+
+Lr={'1PerNiente' '2PocoFav' '3Indiff' ...
+'4Fav' '5MoltoFav'};
+Lc={'1LicElem' '2LicMedia' '3Diploma' ...
+'4LaureaTri' '5LaureaSpe' '6Dottorato'};
+
 % il name,value 'datamatrix',true consente di specificare
 % che l'input è una matrice dei dati e di conseguenza la routine deve
 % calcolare internamente la tabella di contingenza
@@ -22,12 +28,28 @@ Xchk=xlsread('scienza.xlsx','dati','A2:B872','basic');
 % Dato che Xchk è un array 
 outchk=CorAna(Xchk,'datamatrix',true,'Lr',Lr,'Lc',Lc);
 
+%% Contribution biplot
+plots=struct;
+plots.alpha='rowgreen';
+CorAnaplot(out,'plots',plots)
+
+%% Etichette con colore che dipende dal contributo dei punti alla spiegazione dell'inerzia
+plots=struct;
+plots.ColorMapLabelCols='CntrbPnt2In'; 
+CorAnaplot(out,'plots',plots)
+% print -depsc figs/scienzaeduCMAP.eps;
+
+
 %% Per tracciare gli ellissi di confidenza
 % Di seguito viene aggiunto l'ellisse di confidenza al punto riga "per
 % niente" ed al punto colonna 
-confellipse.selRows=1;
-confellipse.selCols=1;
-confellipse.method={'multinomial'};
+confellipse=struct;
+confellipse.selRows=[1:3 5];
+confellipse.selCols=[];
+confellipse.conflev=0.91;
 CorAnaplot(out,'confellipse',confellipse)
+% print -depsc figs/scienzaeduCE.eps;
 
-
+%% Moonplot
+moonplot(out)
+% print -depsc scienzaeduMP.eps;
