690b5fb8298f129c2cb33034ce3f76e42f1e3ee7
UniprJRC <FSDA@unipr.it>
2023-08-05 06:54:20 +0000

    🔄 synced local 'matlabfiles/' with remote 'matlabMfiles/'

--- matlabfiles/capSerieTemporali/sENSO.m
+++ matlabfiles/capSerieTemporali/sENSO.m
@@ -1,47 +1,63 @@
 url = ('https://www.cpc.ncep.noaa.gov/data/indices/sstoi.indices');
 websave('sstoi.txt',url)
-data  = readtable('sstoi.txt', VariableNamingRule='preserve');  % Importazione dei dati in formato 'table'
+data  = readtable('sstoi.txt', VariableNamingRule='preserve');  
 t1    = datetime(data.YR(1),data.MON(1),31);  % data iniziale 
 n     = size(data,1);                         % n.ro delle osservazioni
 t     = t1 +  calmonths(0:n-1);               % vettore di date 
 y     = data.ANOM_3;    % selezione della serie delle anomalie regione 3.4
-g = figure;
-line([t(1) t(end)], [0 0], 'LineStyle','--','Color', 'r'); hold off;
+plot(t, y, 'LineWidth',2);
+yline(0,'Color','r','LineStyle','--')
 xlim([t(1)-calmonths(6), t(end)+calmonths(6)]);
-set(gca,'TickLabelInterpreter','latex');
-exportgraphics(g,'enso.eps');
-exportgraphics(g,'enso.pdf')
+% exportgraphics(g,'enso.eps');
+% exportgraphics(g,'enso.pdf')
+
 %% sample ACF
  % alternatively use this code
 K = 5;
-ylags = [y, NaN(n,K)];
+Ylags = [y, NaN(n,K)];
 for k = 1:K
-    ylags(k+1:end,k+1) = y(1:n-k);
+    Ylags(k+1:end,k+1) = y(1:n-k);
 end
-%
-C = corr(ylags, rows="pairwise")
+% Modo alternativo per ottenere la matrice sfasata
+YlagsCHK=lagmatrix(y,0:5);
+
+%% Autocorrelazioni 
+% con denominatore della covarianza n-k
+C = corr(Ylags, rows="pairwise");
+% con denominatore della covarianza n
+rho = autocorr(y,K);
+autoC=[C(:,1) rho];
+nam=["Autocorr con denom (n-k) nella cov" "Autocorrelazioni"];
+disp(array2table(autoC,"VariableNames",nam))
+
+
+%% Autorrelazioni tramite GUIautocorr
+GUIautocorr(y,'lag',2)
+
+%% 
 bar(0:K, C(1,:))
-r = fSampleACF( y, K )
+
+r = fSampleACF( y, K );
+K = 45;
 bar(0:K, r)
 g = figure;
-K = 45
-r = autocorr(y,K)
 bar(0:K, r);
 %%
 g = figure;
 autocorr(y, 45); %title({'Autocorrelazione'}, Interpreter="latex");
 title('')
-set(gca,'TickLabelInterpreter','latex'); xlabel(' ', Interpreter='latex')
+set(gca,'TickLabelInterpreter','latex');
+xlabel(' ', Interpreter='latex')
 ylabel(' ')
-exportgraphics(g,'gacf.pdf')
+% exportgraphics(g,'gacf.pdf')
 
-%% Ljung-Box 
-[Decision, pValue, LBstat, CriticaValue] = lbqtest(y, 'Lags', 10) 
+
+
 %% Periodogramma via regressione lineare 
 warning('off')
 Perio0 = NaN(floor(n/2),1);
 t   = (1:n)';
-for j = 1:floor(n/2);
+for j = 1:floor(n/2)
     omega_j = 2*pi*j/n;
     mdl = fitlm([cos(omega_j*t) sin(omega_j*t)], y); 
     ESS = mdl.SSR;
@@ -50,6 +66,7 @@ end
 omega = 2 * pi * (1:floor(n/2))' / n; % Frequenze di Fourier
 bar(omega, Perio0); xlim([0 pi]); xlabel('\omega'); ylabel('I(\omega)');
 set(gca,'TickLabelInterpreter','latex');
+
 %% Periodogram via Fast Fourier Transform
 vj = (0:floor(n/2))'; omega = 2 * pi * vj / n; % Frequenze di Fourier
 Perio = abs(fft(y)).^2/(n*2*pi) ;
@@ -62,7 +79,12 @@ g = figure;
 bar(omega, Perio); xlim([0 pi]); 
 xlabel('$\omega$', Interpreter='latex'); ylabel('$I(\omega)$', Interpreter='latex'); 
 set(gca,'TickLabelInterpreter','latex');
-exportgraphics(g,'gPerio.pdf')
+% exportgraphics(g,'gPerio.pdf')
+
 %% esercizio
 plot(2*pi./ omega, Perio)
 [omega Perio]
+
+
+%% Ljung-Box 
+[Decision, pValue, LBstat, CriticaValue] = lbqtest(y, 'Lags', 10) 
