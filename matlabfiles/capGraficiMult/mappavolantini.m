09acf0268f7d43c8abeb463e23a5e32af7e1c5f9
UniprJRC <FSDA@unipr.it>
2023-08-19 10:46:54 +0000

    🔄 synced local 'matlabfiles/' with remote 'matlabMfiles/'

---
+++ matlabfiles/capGraficiMult/mappavolantini.m
@@ -0,0 +1,20 @@
+% Carica i dati della distribuzione dei volantini
+volantini=readtable("dist.volantini.parma.2019.csv");
+summary(volantini)
+% Ci sono 4 categorie di edificio 
+% Nelle coordinate in cui si ditribuisce si collocano almeno 30 volantini
+% e al massimo 48
+
+totvola = sum(volantini.nvol);
+%Il totale dei volantini distribuiti ammonta a quasi 44000
+
+% converte il tipo di edificio in modo categorico per la legenda
+% da usare nella mappa
+volantini.building=categorical(volantini.building);
+% crea il grafico con il titolo che può essere ingrandito o rimpicciolito
+% con il mouse. L'opzione 'ColorData', identifica il tipo di edificio 
+% con opportuno colore e aggiunge la legenda di tale specificazione
+geobubble(volantini.Y, volantini.X, volantini.nvol,'Basemap','streets','ColorData',volantini.building)
+title('Mappa della distribuzione volantini');
+set(gca,'FontSize',15)
+
