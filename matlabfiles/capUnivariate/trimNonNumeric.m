8349d339dfdce30949564e0b2951775ce6c15f13
UniprJRC <FSDA@unipr.it>
2023-08-26 15:54:53 +0000

    🔄 synced local 'matlabfiles/' with remote 'matlabMfiles/'

--- matlabfiles/capUnivariate/trimNonNumeric.m
+++
@@ -1,7 +0,0 @@
-miofile="quarterlyFinances1999To2019.csv";
-opts = detectImportOptions(miofile);
-preview(miofile,opts)
-
-
-opts = detectImportOptions(miofile,'TrimNonNumeric',true);
-preview(miofile,opts)
