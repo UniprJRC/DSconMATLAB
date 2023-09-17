abb5e4f318b2b1e9e0095e9a6f753b341aa7f685
UniprJRC <FSDA@unipr.it>
2023-09-17 14:01:52 +0000

    🔄 synced local 'matlabfiles/' with remote 'matlabMfiles/'

--- matlabfiles/capUnivariate/tabellePIVOT.m
+++ matlabfiles/capUnivariate/tabellePIVOT.m
@@ -1,11 +1,14 @@
+%% Caricamento dati
+
 miofile="Firm.xlsx"; % Caricamento file Firm.xlsx dentro MATLAB 
 X=readtable(miofile,"ReadRowNames",true);
 
-%% Costruzione tablle pivot tramite la funzion pivot (SOLO MATLAB 2023A)
+%% Costruzione tablle pivot tramite la funzion pivot (da MATLAB 2023A)
 
 % Variabile Gender sulle righe (nessuna variabile sulle colonne)
 pivot(X,'Rows','Gender')
 
+% Le modalità della variabile sulle righe sono inseriti come RowNames
 pivot(X,'Rows','Gender','RowLabelPlacement','rownames')
 
 % Variabile Gender sulle righe, Variabile Education sulle colonne
@@ -16,6 +19,12 @@ pivot(X,'Rows','Gender','Columns','Education')
 % Come la precedente ma con l'aggiunta della riga dei totali per righe
 % e colonne
 pivot(X,'Rows','Gender','Columns','Education','IncludeTotals',true)
+
+% Come la precedente ma ora le modalità della variabile
+% sulle righe sono inserite come RowNames
+pivot(X,'Rows','Gender','Columns','Education','IncludeTotals',true, ...
+    'RowLabelPlacement','rownames')
+
 
 % Variabile Gender sulle righe, Variabile Education sulle colonne
 % All'interno la media della retribuzione
@@ -30,19 +39,18 @@ pivot(X,'Rows','Gender','Columns','Education', ...
 % All'interno sia il minimo sia il massimo  della retribuzione
 method=@(x)[min(x) max(x)];
 pv3=pivot(X,'Rows','Gender','Columns','Education', ...
-    'DataVariable','Wage','Method',method)
+    'DataVariable','Wage','Method',method);
+disp(pv3)
 
 % Variabile retribuzione sulle righe. Variabile Education sulle colonne
 % La variabile sulle righe viene suddivisa in 5 classi
 pivot(X,'Rows','Wage','Columns','Education','RowsBinMethod',5)
 
-%% Inizio soluzione esercizio sulle tabelle pivot
+%% Inizio soluzione esercizio 3.9
 
 X1=X; 
 X1.Wage(1)=2000;
 
-% Per specificare le classi con gli estremi richiesti sulle righe occorre
-% che 'RowsBinMethod' contenga gli estremi delle classi
 disp('Tabella pivot punto 1)')
 pv1=pivot(X1,'Rows','Wage','Columns','Education', ...
     'RowsBinMethod',1500:500:4500);
@@ -70,6 +78,13 @@ pv4=pivot(X1,'Rows',{'Wage' 'Seniority'},'Columns','Education', ...
     'RowsBinMethod',{1500:1000:4500 0:15:45});
 disp(pv4)
 
+% Parte non presente nel libro
+% In alternativa ad un cell array i due criteri di raggruppamento possono
+% essere inseriti in uno string array
+pv4chk=pivot(X1,'Rows',["Wage" "Seniority"],'Columns','Education', ...
+    'RowsBinMethod',{1500:1000:4500 0:15:45});
+assert(isequal(pv4,pv4chk),"Cell arrary o array di stringhe non corretti")
+
 % Se una variabile è già qualitativa e non deve essere raggruppata in
 % classi utilizzare 'none'
 disp('Tabella pivot punto 5)')
@@ -78,6 +93,7 @@ pv5=pivot(X1,'Rows',{'Gender' 'Seniority'},'Columns','Education', ...
     'DataVariable','Wage','Method','median');
 disp(pv5)
 
+%% Inizio Esercizio 3.10
 % Se c'è più di una variabile sulle colonne, MATLAB crea tabelle nested
 % una dentro l'altra 
 disp('Tabella pivot pv6')
@@ -89,6 +105,7 @@ disp('Nomi delle variabili di pv6')
 disp(pv6.Properties.VariableNames)
 
 disp('Tabella pivot pv7')
+
 % Per evitare di creare tabelle nested e concatenare i nomi delle modalità
 % delle due variabili inserite nelle colonne, occorre specificare 
 % 'OutputFormat','flat'
@@ -100,5 +117,12 @@ disp('Nomi delle variabili di pv7')
 disp(pv7.Properties.VariableNames)
 
 
-% pivot(X,'Rows','Gender','Columns','Education','OutputFormat','flat')
+%% Parte di seguito non libro
+% Aggiunta opzione 'RowLabelPlacement','rownames'
+pv8=pivot(X1,'Columns',{'Gender' 'Seniority'},'Rows','Education', ...
+    'ColumnsBinMethod',{'none' [0 20 45]}, ...
+    'DataVariable','Wage','Method','min','OutputFormat','flat', ...
+    'RowLabelPlacement','rownames');
+disp(pv8)
+
 
