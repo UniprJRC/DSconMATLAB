bcad450b953e3915c48ece51d8c540f540e663e7
UniprJRC <FSDA@unipr.it>
2023-09-27 21:36:23 +0000

    🔄 synced local 'matlabfiles/' with remote 'matlabMfiles/'

--- matlabfiles/capImportazioni/readQuartelyFinances.m
+++ matlabfiles/capImportazioni/readQuartelyFinances.m
@@ -1,5 +1,10 @@
 %% File di corredo per l'esercizio 4.3
 
+% OSSERVAZIONE: PER POTER CARICARE IL FILE CSV DI SEGUITO OCCORRE CHE LA
+% DOCUMENTAZIONE SIA STATA INSTALLATA IN LOCALE
+% In Home|Preferences|Help
+% Document Location deve essere "Installed Locally"
+
 %% Guardo in anteprima l'output del caricamentod dati
 miofile="quarterlyFinances1999To2019.csv";
 opts = detectImportOptions(miofile);
