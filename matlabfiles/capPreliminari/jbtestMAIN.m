216e9a6f7cb5e8dfd101ccdd08a18d2c7457de9c
UniprJRC <FSDA@unipr.it>
2023-10-11 08:20:14 +0000

    🔄 synced local 'matlabfiles/' with remote 'matlabMfiles/'

--- matlabfiles/capPreliminari/jbtestMAIN.m
+++ matlabfiles/capPreliminari/jbtestMAIN.m
@@ -2,6 +2,13 @@ n=100;
 x=rand(n,1);
 % Calcolo manuale JBSTAT
 z = zscore(x,1); 
+
+% Osservazione: (non inserita nel libro di testo) la chiamata a zscore è
+% con il secondo argomento settato a 1, cioè la standard deviation che
+% viene utilizzata per calcolare lo scostamento standardizzato ha come
+% denominatore n anziché n-1 (v. la definizione di z_i a p. 219 sotto
+% l'equazione (6.1)
+
 AS = sum(z.^3)/n;
 KU = sum(z.^4)/n - 3;
 jbstatM = n*(AS^2/6 + KU^2/24);
