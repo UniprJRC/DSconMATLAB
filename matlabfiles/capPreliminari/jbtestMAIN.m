81a50e19b7ec765ad07fd73b92510a98e4f4ea5a
UniprJRC <FSDA@unipr.it>
2023-10-13 22:10:01 +0000

    🔄 synced local 'matlabfiles/' with remote 'matlabMfiles/'

--- matlabfiles/capPreliminari/jbtestMAIN.m
+++ matlabfiles/capPreliminari/jbtestMAIN.m
@@ -12,7 +12,7 @@ z = zscore(x,1);
 AS = sum(z.^3)/n;
 KU = sum(z.^4)/n - 3;
 jbstatM = n*(AS^2/6 + KU^2/24);
-% Calcolo del 
+% Calcolo del p-value asintotico del test 
 pvalAS=chi2cdf(jbstatM,2,"upper");
 % Calcolo tramite chiamata alla funzione jbtest
 [~,~,jbstat]=jbtest(x);
