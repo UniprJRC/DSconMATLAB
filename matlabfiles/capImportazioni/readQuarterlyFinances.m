9406a23997f37f61b8702bf1b104cdd780e8f12e
UniprJRC <FSDA@unipr.it>
2024-10-02 07:41:13 +0000

    🔄 synced local 'matlabfiles/' with remote 'matlabMfiles/'

--- matlabfiles/capImportazioni/readQuarterlyFinances.m
+++ matlabfiles/capImportazioni/readQuarterlyFinances.m
@@ -5,13 +5,14 @@
 % In Home|Preferences|Help
 % Document Location deve essere "Installed Locally"
 
-%% Guardo in anteprima l'output del caricamentod dati
+%% Guardo in anteprima l'output del caricamento dati
 miofile="quarterlyFinances1999To2019.csv";
 opts = detectImportOptions(miofile);
 preview(miofile,opts)
 
 %% Mostro il formato di caricamento delle variabili
 disp(opts.VariableTypes)
+
 
 
 %% detectImportOptions con l'opzione 'TrimNonNumeric'
