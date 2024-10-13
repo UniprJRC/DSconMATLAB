4a267650007df7e85ee3c873f119e83246770e02
UniprJRC <FSDA@unipr.it>
2024-10-13 19:16:22 +0000

    🔄 synced local 'matlabfiles/' with remote 'matlabMfiles/'

--- matlabfiles/capGraficiMult/graficoGeoBubble.m
+++ matlabfiles/capGraficiMult/graficoGeoBubble.m
@@ -7,6 +7,12 @@ nam=CIT.Properties.RowNames;
 [~,ind]=sort(nam);
 CITord=CIT(ind,:);
 
+% Modo alternativo non riportato nel libro
+% Chiamata a sortrows con secondo argomento "RowNames"
+CITORDchk=sortrows(CIT,"RowNames");
+assert(isequal(CITord,CITORDchk),"Diverso ordinamento")
+
+
 % Aggiungo alla table COO la variabile addedval standardizzata
 % Chiamo la nuova variabile valagg
 COO.valagg=zscore(CITord.addedval);
