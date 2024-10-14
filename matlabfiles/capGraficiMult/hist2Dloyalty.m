66792b95af572f19d0e0020d9f9e2cf85af8c9f9
UniprJRC <FSDA@unipr.it>
2024-10-14 10:42:45 +0000

    🔄 synced local 'matlabfiles/' with remote 'matlabMfiles/'

--- matlabfiles/capGraficiMult/hist2Dloyalty.m
+++ matlabfiles/capGraficiMult/hist2Dloyalty.m
@@ -3,13 +3,13 @@ y=loyalty(:,4);
 X=loyalty(:,1:3);
 histogram2(X(:,1),y,20)
 xlabel("Numero di visite al supermercato")
-ylabel("Ammmontare speso")
+ylabel("Ammontare speso")
 zlabel("Frequenze")
 % print -depsc LOYhist2D.eps;
 
 figure;
 scatter(X(:,1),y)
 xlabel("Numero di visite al supermercato")
-ylabel("Ammmontare speso")
+ylabel("Ammontare speso")
 % print -depsc LOYscatter.eps;
 cascade
