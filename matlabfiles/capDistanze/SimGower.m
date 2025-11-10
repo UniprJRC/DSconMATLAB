766ae015955af434b9208ba9913c51fa797433c2
UniprJRC <FSDA@unipr.it>
2025-11-10 23:09:12 +0000

    🔄 synced local 'matlabfiles/' with remote 'matlabMfiles/'

--- matlabfiles/capDistanze/SimGower.m
+++ matlabfiles/capDistanze/SimGower.m
@@ -1,6 +1,10 @@
 %% Calcolo matrice indici di Gower
-X =[1 1 1 1 1 5	169	450; 1 1 1 1 0 4 94	410; 0 1 0 1 0 4 48	330;
-    1 1 0 1 0 4	66	330; 0 0 0 0 0 3 32	190; 0 0 0 1 0 3 33	165];
+X =[1 1 1 1 1 5	169	450; 
+    1 1 1 1 0 4 94	410; 
+    0 1 0 1 0 4 48	330;
+    1 1 0 1 0 4	66	330;
+    0 0 0 0 0 3 32	190; 
+    0 0 0 1 0 3 33	165];
 l=[2*ones(1,5) ones(1,3)];
 rowlab=string(('A':'F')');
 % La funzione GowerIndex accetta anche un input di tipo table
